3bec041ddef202ada35b76f708c119ac867d46c6
VadimDraganyuk <vadim.draganyuk@gmail.com>
2012-05-31 10:37:21 +0300

    SQLite

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe User do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
 end
